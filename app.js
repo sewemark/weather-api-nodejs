702eaf9773049fed2bcb36e016d0494e58edf0ed
sewemark <sewerynjakub.markowicz@gmail.com>
2017-08-19 18:00:34 +0200

    Query params validation added

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ var express = require('express');
 var app = express();
 const port =3000;
 
-app.get('/',function (req,res) {
-   res.send("Default route");
+function validateCity(city) {
+   return typeof city != 'undefined' && city.trim().toLowerCase()==="londyn" || city.trim().toLowerCase() ==='newyork';
+}
+
+function validateEmail(email) {
+    var reqex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return  typeof email != 'undefined' && reqex.test(email);
+}
+
+function paramsValidator(params) {
+    return validateCity(params.city) && validateEmail(params.email);
+}
+
+app.get('/weather',function (req,res) {
+      if(paramsValidator(req.query)){
+         res.status(200).send('Query params ok')
+      }else{
+         res.status(400).send('Bad request, bad query params');
+      }
 });
 
 app.listen(port);
